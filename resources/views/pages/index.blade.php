227673611f9d207e398864bd6eb7500d1e7d20ba
devika30 <olkardevika69@gmail.com>
2019-09-16 20:05:14 +0530

    adding blog website

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+<div class="jumbotron text-center text-monospace text-capitalize">
+<div class="container">
+<h1 class="text-center">{{$title}}</h1>
+
+   </div>
+   </div>
+   </div>
+@endsection
